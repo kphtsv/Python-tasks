9523c0185c7b4d24a9049d130598c5c75f440e1f
kphtsv <k.phtsv@ya.ru>
2023-05-03 13:33:03 +0500

    + image processor tests

--- ASCII-Art/tests/image_processor_test.py
+++ ASCII-Art/tests/image_processor_test.py
@@ -3,51 +3,20 @@ from PIL import Image
 import image_processor
 
 
-class TestImageProcessor(unittest.TestCase):
-    # def test_on_edges(self):
-    #     self.a
-
-    def test_pixel_brightness(self):
-        row = ((1, 0), (1, 0.5))
+class ImageProcessorTest(unittest.TestCase):
+    def test_flatten_slice(self):
+        rgb_img = Image.open('../images/small_image_bw.png').convert('RGB')
+        row = ((0, 0.5), (2, 0.5))
         column = ((1, 0), (1, 0.5))
-        img = Image.open('TestImages/small_image.png')
-        rgb_img = img.convert('RGB')
-        pixel_coord = (1, 1)
 
-        self.assertAlmostEqual(image_processor.calc_pixel_brightness(row, column, rgb_img, pixel_coord), 0.25)
+        supposed_brightness = 0.25 + 0.5 * 0.5 + 0.25 * 0.6
+        self.assertAlmostEqual(supposed_brightness, image_processor.flatten_slice(row, column, rgb_img), delta=1e-2)
 
-    def rgb_brightness_test():
-        img = Image.open('tests/TestImages/small_image.png')
-        rgb_img = img.convert('RGB')
-        pixel_coord = (1, 4)
-        r, g, b = rgb_img.getpixel(pixel_coord)
+    def test_process(self):
+        filename_in = '../images/small_image_bw.png'
+        str_len = 4
 
-        print(rgb_to_brightness(r, g, b))
+        supposed_art = 'vv``\n++vv\nvvff\nccvv\nvvbb\n$$vv\n'
+        actual_art = image_processor.process(filename_in, str_len, False)
+        self.assertEqual(supposed_art, actual_art)
 
-    def flatten_slice_test():
-        img = Image.open('tests/TestImages/test.png')
-        rgb_img = img.convert('RGB')
-
-        row = ((2, 0), (3, 0))
-        column = ((0, 0), (6, 1))
-
-        print(flatten_slice(row, column, rgb_img))
-
-    def test1():
-        row = ((0, 0), (1, 1.5))
-        column = ((1, 0), (1, 0.5))
-        img = Image.open('tests/TestImages/small_image.png')
-        rgb_img = img.convert('RGB')
-        pixel_coord = (0, 0)
-
-
-        print(calc_pixel_brightness(row, column, rgb_img, pixel_coord))
-
-    def test2():
-        row = ((0, 0), (1, 0))
-        column = ((1, 0.5), (2, 0))
-        img = Image.open('tests/TestImages/small_image.png')
-        rgb_img = img.convert('RGB')
-        pixel_coord = (1, 0)
-
-        print(pixel_is_inside(pixel_coord, row, column))
